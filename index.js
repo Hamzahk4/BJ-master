7b48f62a2028fdc5dc4a94c5965757b89e3133a4
HamzahK <hamzahk4@nycstudents.net>
2024-10-22 11:40:55 -0400

    Yes

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 // Build a BlackJack Game
 
 // variables representing two cards 
-let firstCard = 10
-let secondCard = 4
+let firstCard = 10;
+let secondCard = 20;
+
 
 //challenge #1
 //create a new variable and set it to the sum of the two cards 
-
+let sum = firstCard+secondCard;
 
 //chalenge #2 
 // code the statements below based on the conditions shown in blackjack.png from the images folder
 // your output should work in the browser's console based on changing the values assigned to the cards
 
 //starter code
-    message = "Do you want to draw a new card? 🙂"
-    message = "Wohoo! You've got Blackjack! 🥳"
-    message = "You're out of the game! 😭"
-console.log(message)
+if(sum < 21){
+    message = "Do you want to draw a new card? 🙂";}
+if(sum === 21){
+    message = "Wohoo! You've got Blackjack! 🥳";}
+if(sum > 21){
+    message = "You're out of the game! 😭";}
+
+console.log(message);
